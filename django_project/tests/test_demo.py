083bb0f57a64f41873281df66f51cbded918f4b6
Randy Laptop <Randy.Williams@connxai.com>
2023-06-08 12:58:44 -0700

    Initial commit

---
+++ django_project/tests/test_demo.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+class DemoTestClass(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_this_will_pass(self):
+        self.assertFalse(False)
+
+    def test_this_will_fail(self):
+        self.assertTrue(False)
